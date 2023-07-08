82efc4a1531f9a9cbfdaedf687397ace518f7383
Victor Shevtsov <victorshevtsov@gmail.com>
2023-07-08 23:30:54 +0000

    update index.js

--- valdator/index.js
+++ valdator/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-   res.send("Hello from Validator");
+app.get('/', function (req, res) {
+  res.send(`Hello from Validator\n ${JSON.stringify(process.env, null, 2)}`);
 });
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
   console.log("Listening on port 3000...");
 });
